3c31fb87d31488287b1807db279a47c121f04369
framsey <ferrylin@126.com>
2017-07-25 11:38:57 +0800

    first commit

---
+++ processorder.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>bob's auto parts-order results</title>
+</head>
+<body>
+	<h1>bob's auto parts</h1>
+	<h2>order results</h2>
+	<?php 
+	echo "<p>order processed at ";
+	echo date(' H:i, jS F Y');
+	echo "</p>";
+	 ?>
+</body>
+</html>
